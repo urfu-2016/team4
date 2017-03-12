ef653f00af4e5db43cb7e210ded5cef1a246b038
Трефилов <trefilovandrew@yandex.ru>
2017-03-12 15:59:33 +0500

    change files structure

--- app.js
+++ app.js
@@ -16,11 +16,11 @@ var mongoOpt = {
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://admin:1qazXSW@ds129090.mlab.com:29090/yaheckaton', mongoOpt);
 
-var routers = require('./controllers/routes');
+var routers = require('./src/controllers/routes');
 
 routers.initRouters(app);
 
-app.listen(app.get('port'), () => {
+app.listen(app.get('port'), function(){
     console.log('Server start on ' +
         app.get('port') +
         '. Ctrl + C for exit.');
