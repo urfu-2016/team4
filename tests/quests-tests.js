da14742a6722182c509a2bd04eb4f36510514e11
Atnes Ness <atnesness@gmail.com>
2017-03-24 12:29:14 +0500

    Линтинг тестов

--- tests/quests-tests.js
+++ tests/quests-tests.js
@@ -1,7 +1,7 @@
+/* eslint quote-props: ["error", "as-needed", { "keywords": true, "unnecessary": false }] */
 const assert = require('assert');
 const parseQuery = require('../src/controllers/query-parser');
 describe('quests', () => {
-
     it('test parseQuery function', () => {
         assert.deepEqual(parseQuery({
             'Model__field_name': 'value',
@@ -9,27 +9,26 @@ describe('quests', () => {
             'Model__opt__limit': 10,
             'Model__field2__include': 'includeValue',
             'AnotherModel__field_sub_name__gt': 1,
-
-            'AnotherModel__field_sub_name2__include': 'includeValue',
+            'AnotherModel__field_sub_name2__include': 'includeValue'
         }),
             {
-            'Model': {
-                find: {
-                    'field.name': 'value',
-                    'field2': /.*includeValue.*/,
+                'Model': {
+                    find: {
+                        'field.name': 'value',
+                        'field2': /.*includeValue.*/
+                    },
+                    options: {
+                        'sort': {'field2': 1},
+                        'limit': 10
+                    }
                 },
-                options: {
-                    'sort': {'field2': 1},
-                    'limit': 10
+                'AnotherModel': {
+                    find: {
+                        'field.sub.name': {'$gt': 1},
+                        'field.sub.name2': /.*includeValue.*/
+                    },
+                    options: {}
                 }
-            },
-            'AnotherModel': {
-                find: {
-                    'field.sub.name': {'$gt': 1},
-                    'field.sub.name2': /.*includeValue.*/
-                },
-                options: {}
-            }
-        });
+            });
     });
 });
