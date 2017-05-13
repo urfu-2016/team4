89b4cf0c8b613b004ee224b7f2db35eee0932b9e
Sergey Ubogov <sergeiubogov@mail.ru>
2017-05-14 00:01:34 +0500

    валидация на клиенте (#122)
    * валидация на клиенте
    
    * исправил багу с тем что флаг публикации ставился в любом случае, и можно было по нажатию на кнопку сохранить опубликовать квест
    
    * при нулевом количестве квестов нельзя запостить, на удаленных фотках можно не ставить локацию

--- src/blocks/create-quest-page/create-quest-page.js
+++ src/blocks/create-quest-page/create-quest-page.js
@@ -7,10 +7,41 @@ let publishInput = block.querySelector('.publish');
 let nameInput = block.querySelector('.create-quest-page-quest-title input');
 let counter = block.querySelector('.counter');
 
-publishBtn.addEventListener('click', () => {
-    publishInput.checked = 'true';
+function getCountDeletedPhotos() {
+    return [].slice.apply(photoListBlock.querySelectorAll('.quest-photo'),
+        [0, counter.getAttribute('value')])
+        .reduce((total, questPhoto) => {
+            return total + Number(questPhoto.querySelector('.deleted').checked);
+        }, 0);
+}
+
+publishBtn.addEventListener('click', e => {
+    let checkMissingLocation = [].slice.apply(photoListBlock.querySelectorAll('.quest-photo'),
+        [0, counter.getAttribute('value')]).some(questPhoto => {
+            if (questPhoto.querySelector('.deleted').checked) {
+                return false;
+            }
+            let location = questPhoto
+                .querySelector('.photo-location')
+                .getAttribute('value')
+                .split(', ')
+                .map(coord => Number(coord));
+
+            return location[0] === 0 && location[1] === 0;
+        });
+    let countDeletedPhoto = getCountDeletedPhotos();
+    if (checkMissingLocation) {
+        createFlashMessage('Укажите местоположение для каждого фото!', 'error');
+        e.preventDefault();
+    } else if (Number(counter.value) - countDeletedPhoto === 0) {
+        createFlashMessage('Не загружено ни одного изображения', 'error');
+        e.preventDefault();
+    } else {
+        publishInput.checked = 'true';
+    }
 });
 form.addEventListener('submit', e => {
+    let countDeletedPhoto = getCountDeletedPhotos();
     if (photoListBlock.hasImageSet()) {
         if (!confirm('Незагруженные изображения не будут сохранены')) {
             e.preventDefault();
@@ -19,7 +50,7 @@ form.addEventListener('submit', e => {
     if (!nameInput.value) {
         createFlashMessage('Название квеста не должно быть пустым', 'error');
         e.preventDefault();
-    } else if (!Number(counter.value)) {
+    } else if (!(Number(counter.value) - countDeletedPhoto)) {
         createFlashMessage('Не загружено ни одного изображения', 'error');
         e.preventDefault();
     }
